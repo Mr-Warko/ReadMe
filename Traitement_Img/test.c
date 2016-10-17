e32103ab9902558baa7041423b28423b35bed883
Li3wish <alexandre.bernard@epita.fr>
2016-10-17 21:26:28 +0200

    Revert "Revert "Ajout de la sauvegarde de char. À améliorer.""
    This reverts commit 5590dbc46c7d94a738b4b24ece484cd015094f71.

--- Traitement_Img/test.c
+++ Traitement_Img/test.c
@@ -1,39 +1,94 @@
 #include <stdlib.h>
 #include <stdio.h>
-//#include "img.c"
-/*
-int main(){
 
-	struct list *l = empty_list();
-	int array[] = {0,1,2,3};
-	l = list_append(l, array);
-	int array2[] = {4,5,6,7};
-	l = list_append(l, array2);
-	print_list(l);	
-	return 0;
+#include <err.h>
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
 
 
-	struct list *l = empty_list();
-	int array2[] = {7,1,2,3};
-	l = add(l,array2);
-	for(int i = 0; i < 6; ++i){
-		int *array = malloc(4*sizeof(int));
-		*array = 0;
-		*(array) += i; *(array+1)=1; *(array+2)=2; *(array+3)=3;
-		l = insert(l,array);
-	}
-	print_list(l);
+void wait_for_keypressed(void) {
+  SDL_Event             event;
+  // Infinite loop, waiting for event
+  for (;;) {
+    // Take an event
+    SDL_PollEvent( &event );
+    // Switch on event type
+    switch (event.type) {
+    // Someone pressed a key -> leave the function
+    case SDL_MOUSEBUTTONUP: return;
+    default: break;
+    }
+  // Loop until we got the expected event
+  }
 }
 
-	struct list *l = NULL;
-	int array[] = {1};
-	l = add(l,array);
-	struct list **j = &l;
-	int i = &(*j)->value;
-	printf("**j = %i \n",**j);
-	printf("j = %i \n", *((*j)->value));
-}*/
+void init_sdl(void) {
+  // Init only the video part
+  if( SDL_Init(SDL_INIT_VIDEO)==-1 ) {
+    // If it fails, die with an error message
+    errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+  }
+  // We don't really need a function for that ...
+}
 
-int main(int argc, int *argv[]){
-call(argv[1]);
-return 0; }
+SDL_Surface* display_image(SDL_Surface *img) {
+  SDL_Surface          *screen;
+  // Set the window to the same size as the image
+  screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+  if ( screen == NULL ) {
+    // error management
+    errx(1, "Couldn't set %dx%d video mode: %s\n",
+         img->w, img->h, SDL_GetError());
+  }
+ 
+  /* Blit onto the screen surface */
+  if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+    warnx("BlitSurface error: %s\n", SDL_GetError());
+ 
+  // Update the screen
+  SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+ 
+  // wait for a key
+  wait_for_keypressed();
+ 
+  // return the screen for further uses
+  return screen;
+}
+
+int main()
+{ 
+
+	SDL_Surface *surface;
+	Uint32 rmask, gmask, bmask, amask;
+	
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    rmask = 0xff000000;
+    gmask = 0x00ff0000;
+    bmask = 0x0000ff00;
+    amask = 0x000000ff;
+#else
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = 0xff000000;
+#endif
+
+	surface = SDL_CreateRGBSurface(SDL_SWSURFACE, 100, 100, 32,
+																 rmask, gmask, bmask, amask);
+	if(surface == NULL) {
+			fprintf(stderr, "CreateRGBSurface failed: %s\n", SDL_GetError());
+			exit(1);
+	}
+	
+	if(SDL_SaveBMP(surface, "Yolo/yolo.bmp") < 0){
+	fprintf(stderr, "saving surfave failed: %s\n", SDL_GetError());
+			exit(1);
+	}
+
+	SDL_FreeSurface(surface);
+
+	printf("a" + "c");
+	return 0;
+}
+
